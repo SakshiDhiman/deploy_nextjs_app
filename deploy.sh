7c1b0b2575e85c9daa65ba0eda467c3edef2f593
SakshiDhiman <49397348+SakshiDhiman@users.noreply.github.com>
2023-06-09 16:48:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-export NVM_DIR=$HOME/.nvm;
-. $NVM_DIR/nvm.sh;
-
 cd /home/ubuntu/deploy_nextjs_app
 git pull origin master
-nvm use v16
-yarn install &&
-yarn build &&
+npm install &&
+npm run build &&
 pm2 restart next_hello_world_app
